6c7ecd448d5c81d87a7c6ca4bdf896c6817482a4
Braxter <bryaam203@gmail.com>
2024-02-29 20:14:30 -0600

    listo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [paymentStatus, setPaymentStatus] = useState('');
+  const wsRef = useRef(null);
+
+  useEffect(() => {
+    if (!wsRef.current) {
+      const socket = new WebSocket('ws://localhost:8080');
+
+
+      socket.onopen = () => {
+        console.log('Conexión WebSocket abierta');
+        wsRef.current = socket; 
+      };
+
+
+      socket.onmessage = (event) => {
+        const message = event.data;
+        setPaymentStatus(message);
+      };
+
+      socket.onerror = (error) => {
+        console.error('Error en WebSocket:', error);
+
+      };
+
+      return () => {
+        if (wsRef.current) {
+          wsRef.current.close();
+        }
+      };
+    }
+  }, []);
+
+  const handleButtonClick = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/pagos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ amount: 1000 }), 
+      });
+  
+      if (response.ok) {
+        console.log('Pago exitoso');
+      } else {
+        console.error('Error al procesar el pago:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error al realizar la solicitud de pago:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pagos</h1>
+        <button onClick={handleButtonClick}>Realizar Pago</button>
+        {paymentStatus && <p>Estado del Pago: {paymentStatus}</p>}
       </header>
     </div>
   );
-}
+};
 
 export default App;
